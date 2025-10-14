45ad081bdb2452f0464b431eff411861105e257b
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-10-13 21:21:06 -0400

    home/index.html is now visible

---
+++ app/routes/home_routes.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template
+
+home_bp = Blueprint('home', __name__,url_prefix='/home')
+
+@home_bp.route('/index')
+def index():
+    return render_template('home/index.html')

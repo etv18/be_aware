7e5184459c329191a3c18255b4f2527dfd5ca0fe
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-10-14 21:42:58 -0400

    create view has been set up

--- app/routes/bank_routes.py
+++ app/routes/bank_routes.py
@@ -2,6 +2,10 @@ from flask import Blueprint, render_template
 
 bank_bp = Blueprint('bank', __name__, url_prefix='/banks')
 
-@bank_bp.route('/index')
+@bank_bp.route('/index', methods=['GET'])
 def index():
     return render_template('banks/index.html')
+
+@bank_bp.route('/create', methods=['GET', 'POST'])
+def create():
+    return render_template('banks/create.html')

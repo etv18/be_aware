c6e1c3e8e68ef54de830a39eece647d44f06b799
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-10-20 00:04:38 -0400

    update method for bank_account_controller

--- app/controllers/bank_account_controller.py
+++ app/controllers/bank_account_controller.py
@@ -19,3 +19,12 @@ def create_bank_account():
 
         db.session.add(bank_account)
         db.session.commit()
+
+def update_bank_account(bank_account):
+    if request.method == 'POST':
+        bank_account.nick_name = request.form['nick_name'];
+        bank_account.account_number = request.form['account_number'];
+        bank_account.bank_id = request.form['bank_id'];
+        bank_account.amount_available = request.form['amount_available'];
+
+        db.session.commit()

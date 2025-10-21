c54d92a787d4fda4a77d0d15c46b9a9264c58e8c
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-10-20 23:58:27 -0400

    credit_cards/index.html allows you to create credit_cards

--- app/routes/credit_card_routes.py
+++ app/routes/credit_card_routes.py
@@ -9,12 +9,15 @@ credit_card_bp = Blueprint('credit_card', __name__, url_prefix='/credit_cards')
 @credit_card_bp.route('/index', methods=['GET'])
 def index():
     banks = Bank.query.all()
-
+    credit_cards = CreditCard.query.all()
     context = {
-        'banks': banks
+        'banks': banks,
+        'credit_cards': credit_cards
     }
     return render_template('credit_cards/index.html', **context)
 
 @credit_card_bp.route('/create', methods=['POST'])
 def create():
-    pass
+    cc_controller.create_credit_card()
+
+    return redirect(url_for('credit_card.index'))

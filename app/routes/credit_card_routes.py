2d1e098deef8b093c13c6cbafb613b2e3ba32ffe
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-10-20 21:35:14 -0400

    credit cards index template set up

---
+++ app/routes/credit_card_routes.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template, redirect, url_for, request
+
+from app.controllers import credit_card_controller as cc_controller
+from app.models.credit_card import CreditCard
+from app.models.bank import Bank
+
+credit_card_bp = Blueprint('credit_card', __name__, url_prefix='/credit_cards')
+
+@credit_card_bp.route('/index', methods=['GET'])
+def index():
+    return render_template('credit_cards/index.html')
+
+@credit_card_bp.route('/create', methods=['POST'])
+def create():
+    pass

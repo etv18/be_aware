9ca58179ccee977ce9edaa6bc9dd344f188135d5
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-10-27 21:13:07 -0400

    system allows you to update expense category's records

---
+++ app/controllers/expense_category_controller.py
@@ -0,0 +1,23 @@
+from flask import request
+
+from app.extensions import db
+from app.models.expense_category import ExpenseCategory
+
+def create_expense_category():
+    if request.method == 'POST':
+        name = request.form['name']
+
+        expense_category = ExpenseCategory(name=name)
+
+        db.session.add(expense_category)
+        db.session.commit()
+
+def update_expense_category(expense_category):
+    if request.method == 'POST':
+        expense_category.name = request.form['name']
+        
+        db.session.commit()
+
+def delete_expense_category(expense_category):
+    pass
+
--- app/routes/expense_category_routes.py
+++ app/routes/expense_category_routes.py
@@ -7,4 +7,21 @@ expense_category_bp = Blueprint('expense_category',__name__, url_prefix='/expens
 
 @expense_category_bp.route('/index', methods=['GET'])
 def index():
-    return render_template('expense_categories/index.html')
+    expense_categories = ExpenseCategory.query.all()
+
+    return render_template('expense_categories/index.html', expense_categories=expense_categories)
+
+@expense_category_bp.route('/create', methods=['GET', 'POST'])
+def create():
+    ec_controller.create_expense_category()
+
+    return redirect(url_for('expense_category.index'))
+
+@expense_category_bp.route('/update', methods=['GET', 'POST'])
+def update():
+    expense_category_id = request.form['id']
+    expense_category = ExpenseCategory.query.get(expense_category_id)
+    if expense_category:
+        ec_controller.update_expense_category(expense_category)
+        
+    return redirect(url_for('expense_category.index'))

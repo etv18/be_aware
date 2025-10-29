be0e58f4893ad5361551bb9aa00437e991521777
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-10-29 00:21:07 -0400

    feature on expenses/index.html that allows you to enable and disable selects depending if you select either a credit card or bank account

--- app/routes/expense_routes.py
+++ app/routes/expense_routes.py
@@ -2,10 +2,20 @@ from flask import request, redirect, url_for, render_template, Blueprint
 
 from app.models.expense import Expense
 from app.controllers import expense_controller
+from app.models import credit_card, expense_category, bank_account
 
 expense_bp = Blueprint('expense', __name__, url_prefix='/expenses')
 
 @expense_bp.route('/index', methods=['GET'])
 def index():
+    credit_cards = credit_card.CreditCard.query.all()
+    expense_categories = expense_category.ExpenseCategory.query.all()
+    bank_accounts = bank_account.BankAccount.query.all()
 
-    return render_template('expenses/index.html')
+    context = {
+        'credit_cards': credit_cards,
+        'expense_categories': expense_categories,
+        'bank_accounts': bank_accounts
+    }
+    
+    return render_template('expenses/index.html', **context)

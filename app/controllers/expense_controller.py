e64be1818278704ccc36eea9251fedb5f101c873
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-11-01 00:51:18 -0400

    the program will save an expense made with ither cash  or a bank product not both at the same time

--- app/controllers/expense_controller.py
+++ app/controllers/expense_controller.py
@@ -10,15 +10,23 @@ def create_expense():
         expense_category_id = int(request.form['select-expense-category'])
         credit_card_id = None
         bank_account_id = None
+        '''
+            this conditional will save either a credit_card_id or a bank_account_id
+            only if you marked the expense as it wasn't made with cash, in other words...
+            
+            the code inside the if statement will be executed ONLY if you on the frontend 
+            turn off the switcher which determines if you did or didn't use cash on the 
+            transaction you just created.
+        '''
+        if not is_cash:
+            selected_credit_card = request.form.get('select-credit-card')
+            selected_bank_account = request.form.get('select-bank-account')
 
-        selected_credit_card = request.form.get('select-credit-card')
-        selected_bank_account = request.form.get('select-bank-account')
+            if selected_credit_card and selected_credit_card != 'none':
+                credit_card_id = int(request.form['select-credit-card'])
 
-        if selected_credit_card and selected_credit_card != 'none':
-            credit_card_id = int(request.form['select-credit-card'])
-
-        if selected_bank_account and selected_bank_account != 'none':
-            bank_account_id = int(request.form['select-bank-account'])
+            if selected_bank_account and selected_bank_account != 'none':
+                bank_account_id = int(request.form['select-bank-account'])
 
         expense = Expense(
             amount=amount,
@@ -39,14 +47,20 @@ def update_expense(expense):
         credit_card_id = None
         bank_account_id = None
 
-        selected_credit_card = request.form.get('select-credit-card')
-        selected_bank_account = request.form.get('select-bank-account')
+        '''
+        if the expense you are updating, you edit it as it wasnt made with
+        cash then the program it'll save either the credit_card_id or 
+        bank_account_id
+        '''
+        if not is_cash:
+            selected_credit_card = request.form.get('select-credit-card')
+            selected_bank_account = request.form.get('select-bank-account')
 
-        if selected_credit_card and selected_credit_card != 'none':
-            credit_card_id = int(request.form['select-credit-card'])
+            if selected_credit_card and selected_credit_card != 'none':
+                credit_card_id = int(request.form['select-credit-card'])
 
-        if selected_bank_account and selected_bank_account != 'none':
-            bank_account_id = int(request.form['select-bank-account'])
+            if selected_bank_account and selected_bank_account != 'none':
+                bank_account_id = int(request.form['select-bank-account'])
 
         expense.amount = amount
         expense.is_cash = is_cash

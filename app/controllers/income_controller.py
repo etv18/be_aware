aeaccbe21b8ff544c70b1d06f4c2ac18f8286194
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-11-02 13:20:37 -0400

    now you can create incomes records

---
+++ app/controllers/income_controller.py
@@ -0,0 +1,22 @@
+from flask import request
+
+from app.models.income import Income
+from app.extensions import db
+
+def create_income():
+    if request.method == 'POST':
+        amount = request.form['amount']
+        is_cash = request.form.get('is-cash') == 'on'
+        bank_account_id = None
+
+        if not is_cash:
+            bank_account_id = int(request.form.get('select-bank-account'))
+            
+        income = Income(
+            amount=amount,
+            is_cash=is_cash,
+            bank_account_id=bank_account_id
+        )
+
+        db.session.add(income)
+        db.session.commit()
--- app/routes/income_routes.py
+++ app/routes/income_routes.py
@@ -1,10 +1,28 @@
 from flask import Blueprint, redirect, render_template, request, url_for
 
-from app.models.income import Income
+from app.models import income, bank_account
+from app.controllers import income_controller
 
 income_bp = Blueprint('income', __name__, url_prefix='/incomes')
 
 @income_bp.route('/index', methods=['GET', 'POST'])
 def index():
+    incomes = income.Income.query.all()
+    bank_accounts = bank_account.BankAccount.query.all()
+
+    context = {
+        'incomes': incomes,
+        'bank_accounts': bank_accounts
+    }
+
+    return render_template('incomes/index.html', **context)
+
+@income_bp.route('/create', methods=['GET', 'POST'])
+def create():
+    income_controller.create_income()
+    return redirect(url_for('income.index'))
+
+@income_bp.route('/update', methods=['POST'])
+def update():
     
-    return render_template('incomes/index.html')
+    return redirect(url_for('income.index'))

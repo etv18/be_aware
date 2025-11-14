1567212daa6f394d354f9c0eaf1d5597c0aee9e7
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-11-13 21:36:39 -0400

    script.js file to get cash flow info from a specific bank account

---
+++ app/static/scripts/bank_accounts/script.js
@@ -0,0 +1,42 @@
+function preventBtnClickWhenClickOnRow(event){
+    return event.target.closest('button') || event.target.closest('form');
+}
+
+async function getAssociatedExpenses(row){
+    let data;
+    try {
+        let bankAccountId = row.getAttribute('data-bank-account-id');
+        const response = await fetch(`/bank_accounts/associated_expenses/${bankAccountId}`);
+        if(!response.ok) {
+            console.log(response.json())
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: data.error || 'Something went wrong while creating the expense.'
+            });
+            return;
+        }
+        data = await response.json();
+        
+    } catch (error) {
+        console.log(response.json())
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: data.error || 'Something went wrong while creating the expense.'
+        });
+    }
+    return data;
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const rows = document.querySelectorAll('.bank-row');
+
+    rows.forEach(row => {
+        row.addEventListener('click', (event) => {
+            if(preventBtnClickWhenClickOnRow(event)) return;
+            console.log('You clicked a row')
+        })
+    });
+});
+

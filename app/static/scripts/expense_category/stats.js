8e90ed70de095bd2069aa47d8d5282dc76f2308c
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-11-20 21:04:54 -0400

    function to fetch expenses category monthly data

--- app/static/scripts/expense_category/stats.js
+++ app/static/scripts/expense_category/stats.js
@@ -1,10 +1,53 @@
+const canvas = document.getElementById('totals-per-category');
+
 async function getDataByCategory(){
     let data;
     try {
-        const response = await fetch(`/expense_categories/monthly/chart`)
+        const response = await fetch(`/expense_categories/monthly/chart`);
+        if(!response.ok) {
+            console.log(response.json());
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: data.error || 'Somethig went wrong while editing the expense.'
+            });
+            return;
+        }
+
+        data = response.json()
     } catch (error) {
         console.error(error);
     }
 
     return data;
 }
+
+function generateMontlyChart(canvas, type, data) {
+    try {
+        new Chart(canvas, {
+            type: type,
+            data: {
+                labels: data.names,
+                datasets: [{
+                    label: 'Amount',
+                    data: data.totals,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {beginAtZero: true}
+                }
+            }
+        });
+    } catch (error) {
+        console.error(error)
+        throw new Error();
+    }
+}
+
+document.addEventListener('DOMContentLoaded',async e => {
+    data = await getDataByCategory();
+    generateMontlyChart(canvas, 'doughnut', data);
+});
+

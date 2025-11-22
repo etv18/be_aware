dccc715d025bc19f9ebdb6a768aa02340935cb8a
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-11-22 15:59:08 -0400

    you have to double click to see associated records for expense category

--- app/static/scripts/expense_category/script.js
+++ app/static/scripts/expense_category/script.js
@@ -5,7 +5,7 @@ function preventBtnClickWhenClickOnRow(event){
 document.addEventListener('DOMContentLoaded', event => {
     const rows = document.querySelectorAll('.expense-category-row');
     rows.forEach(row => {
-        row.addEventListener('click', event => {
+        row.addEventListener('dblclick', event => {
             if(preventBtnClickWhenClickOnRow(event)) return;
             let expenseCategoryId = row.getAttribute('data-expense-category-id');
             window.location.assign(`/expense_categories/associated_records/${expenseCategoryId}`)

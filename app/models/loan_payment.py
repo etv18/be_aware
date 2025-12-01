f42a065df895c06f17f8ca2a8f3eb0c3ce45e0c3
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-11-30 20:31:27 -0400

    loan payment model set up

---
+++ app/models/loan_payment.py
@@ -0,0 +1,16 @@
+from app.extensions import db
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+
+class LoanPayment(db.Model):
+    __tablename__ = 'loan_payment'
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Numeric(10,2), nullable=False)
+    
+    loan_id = db.Column(db.Integer, db.ForeignKey('loans.id'))
+
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())  
+
+    loan = relationship('Loan', back_populates='loan') 
+

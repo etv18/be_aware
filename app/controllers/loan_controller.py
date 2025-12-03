ee9464d37ab96ecb4f50432a3c78e3a209801050
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-12-02 23:35:05 -0400

    controllers

---
+++ app/controllers/loan_controller.py
@@ -0,0 +1,35 @@
+from flask import request
+from sqlalchemy import func
+from sqlalchemy.exc import SQLAlchemyError
+
+from decimal import Decimal
+
+from app.extensions import db
+from app.models.loan import Loan
+from app.exceptions.bankProductsException import AmountIsLessThanOrEqualsToZero
+def create_loan():
+    try:
+        amount = Decimal(request.form.get('amount'))
+        is_cash = request.form.get('is-cash') == 'on'
+        person_name = request.form.get('person-name')
+        bank_account_id = None
+        if(amount <= 0): raise AmountIsLessThanOrEqualsToZero('Introduce a number bigger than 0')
+        if not is_cash:
+            bank_account_id = int(request.form.get('select-bank-account'))
+
+        loan = Loan(
+            amount=amount,
+            is_cash=is_cash,
+            person_name=person_name,
+            bank_account_id=bank_account_id
+        )
+
+        db.session.add(loan)
+        db.session.commit()
+
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        raise e
+    except Exception as e:
+        db.session.rollback()
+        raise e

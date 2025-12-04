b039dc4b3c178160626270e4e92c65d701ff54ac
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-12-03 21:12:23 -0400

    create loan endpoint

--- app/routes/accounts_receivable_routes.py
+++ app/routes/accounts_receivable_routes.py
@@ -1,7 +1,17 @@
 from flask import Blueprint, render_template, redirect, url_for, request, jsonify
+from app.controllers import loan_controller
 
 accounts_receivable_bp = Blueprint('accounts_receivable', __name__, url_prefix='/accounts_receivable')
 
 @accounts_receivable_bp.route('/index', methods=['GET'])
 def index():
     return render_template('accounts_receivable/index.html')
+
+@accounts_receivable_bp.route('/create_loan', methods=['POST'])
+def create_loan():
+    try:
+        loan_controller.create_loan()
+        return jsonify({'message': 'Accounts receivable created successfully'}), 201
+    except Exception as e:
+        print(e)
+        return jsonify({'error': str(e)}), 400

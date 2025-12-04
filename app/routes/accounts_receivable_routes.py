63e1d19d6009a7ff561cd7872063ac08b2ccebda
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-12-03 21:33:10 -0400

    added context variables to be able to show them in the front end

--- app/routes/accounts_receivable_routes.py
+++ app/routes/accounts_receivable_routes.py
@@ -1,11 +1,20 @@
 from flask import Blueprint, render_template, redirect, url_for, request, jsonify
+
 from app.controllers import loan_controller
+from app.models.loan import Loan
+from app.models.bank_account import BankAccount
 
 accounts_receivable_bp = Blueprint('accounts_receivable', __name__, url_prefix='/accounts_receivable')
 
 @accounts_receivable_bp.route('/index', methods=['GET'])
 def index():
-    return render_template('accounts_receivable/index.html')
+    loans = Loan.query.all()
+    bank_accounts = BankAccount.query.all()
+    context = {
+        'loans': loans,
+        'bank_accounts': bank_accounts,
+    }
+    return render_template('accounts_receivable/index.html', **context)
 
 @accounts_receivable_bp.route('/create_loan', methods=['POST'])
 def create_loan():

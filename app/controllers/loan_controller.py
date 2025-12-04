e06d35564284a2d6b56aec9502fcf336ddc1ff8f
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-12-04 15:09:51 -0400

    handler to update a loan record

--- app/controllers/loan_controller.py
+++ app/controllers/loan_controller.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, jsonify
 from sqlalchemy import func
 from sqlalchemy.exc import SQLAlchemyError
 
@@ -6,7 +6,7 @@ from decimal import Decimal
 
 from app.extensions import db
 from app.models.loan import Loan
-from app.controllers.expense_controller import update_bank_account_money_on_create
+from app.controllers.expense_controller import update_bank_account_money_on_create, update_bank_account_money_on_update
 from app.exceptions.bankProductsException import AmountIsLessThanOrEqualsToZero
 def create_loan():
     try:
@@ -17,7 +17,7 @@ def create_loan():
         bank_account_id = None
         
         if(amount <= 0): raise AmountIsLessThanOrEqualsToZero('Introduce a number bigger than 0')
-        
+
         if not is_cash:
             bank_account_id = int(request.form.get('select-bank-account'))
             update_bank_account_money_on_create(bank_account_id, amount)
@@ -39,3 +39,45 @@ def create_loan():
     except Exception as e:
         db.session.rollback()
         raise e
+    
+def update_loan(loan):
+    try:
+        if not loan:
+            return jsonify({'error': 'Loan record was not found'}), 400
+        
+        amount = Decimal(request.form.get('amount'))
+        is_cash = request.form.get('is-cash') == 'on'
+        person_name = request.form.get('person-name')
+        description = request.form.get('description')
+        bank_account_id = None
+        if(amount <= 0): raise AmountIsLessThanOrEqualsToZero('Introduce a number bigger than 0')
+        
+        if not is_cash:
+            bank_account_id = int(request.form.get('select-bank-account'))
+            update_bank_account_money_on_update(loan.bank_account_id, bank_account_id, loan.amount, amount)
+        else:
+            #if the loan is updated from a loan made with a bank account to a loan made with cash
+            #this code will return the money which was used before.
+            loan.bank_account.amount_available += loan.amount 
+
+        loan.amount = amount
+        loan.is_cash = is_cash
+        loan.person_name = person_name
+        loan.description = description
+        loan.bank_account_id = bank_account_id
+    
+        db.session.commit()
+
+    except SQLAlchemyError as e:
+        print(f'Error on update_loan handler: {e}')
+        db.session.rollback()
+        raise e
+    except Exception as e:
+        print(f'Error on update_loan handler: {e}')
+        db.session.rollback()
+        raise e
+    
+    return jsonify({'data': 'Loan updated successfully'}), 201
+
+
+#HELPERS

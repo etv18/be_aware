7bf54a33fa6fe35069944c4be3727df35eb586d5
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-12-04 15:21:17 -0400

    endpoint to delete loans records

--- app/routes/accounts_receivable_routes.py
+++ app/routes/accounts_receivable_routes.py
@@ -3,6 +3,7 @@ from flask import Blueprint, render_template, redirect, url_for, request, jsonif
 from app.controllers import loan_controller
 from app.models.loan import Loan
 from app.models.bank_account import BankAccount
+from app.extensions import db
 
 accounts_receivable_bp = Blueprint('accounts_receivable', __name__, url_prefix='/accounts_receivable')
 
@@ -30,6 +31,15 @@ def update_loan(loan_id):
     try:
         loan = Loan.query.get(loan_id)
         return loan_controller.update_loan(loan)
+    except Exception as e:
+        print(e)
+        return jsonify({'error': str(e)}), 400
+    
+@accounts_receivable_bp.route('/delete/<int:loan_id>', methods=['DELETE'])
+def delete_loan(loan_id):
+    try:
+        loan = Loan.query.get(loan_id)
+        return loan_controller.delete_loan(loan)
     except Exception as e:
         print(e)
         return jsonify({'error': str(e)}), 400

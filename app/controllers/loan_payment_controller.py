5af6176377239a6a2434433b51cb3f25414d847a
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-12-05 16:38:09 -0400

    handler to update loan payments

--- app/controllers/loan_payment_controller.py
+++ app/controllers/loan_payment_controller.py
@@ -5,6 +5,7 @@ from sqlalchemy.exc import SQLAlchemyError
 from decimal import Decimal
 
 from app.extensions import db
+from app.models.bank_account import BankAccount
 from app.models.loan import Loan
 from app.exceptions.bankProductsException import AmountIsLessThanOrEqualsToZero
 from app.controllers.income_controller import update_bank_account_money_on_create, update_bank_account_money_on_update
@@ -51,6 +52,53 @@ def create_loan_payment():
         db.session.rollback()
         raise e
 
+
+def update_loan_payment(loan_payment):
+    try:
+        if not loan_payment:
+            return jsonify({'error': 'Loan payment record was not found'}), 400
+        
+        new_amount = Decimal(request.form['amount'])
+        new_bank_account_id = None
+        is_cash = request.form.get('is-cash') == 'on'
+        selected_bank_account = request.form.get('select-bank-account')
+        loan_id = int(request.form['loan-id'])
+        
+        
+        if(new_amount <= 0): raise AmountIsLessThanOrEqualsToZero('Introduce a number bigger than 0')
+
+        loan = Loan.query.get(loan_id)
+        if not loan:
+            return jsonify({'error': 'Loan record was not found'}), 400
+
+        expected_total_payments = (loan.total_payments() - loan_payment.amount) + new_amount
+        if expected_total_payments > loan.amount:
+            return jsonify({
+                'message': 'The updated amount exceeds the remaining loan balance',
+                'is_paid': True
+            }), 200
+        
+        if not is_cash and (selected_bank_account != None or selected_bank_account != '' or selected_bank_account != 'none'):
+            new_bank_account_id = int(request.form.get('select-bank-account'))
+            new_bank_account = BankAccount.query.get(new_bank_account_id)
+            update_bank_account_money_on_update(loan_payment.bank_account, new_bank_account, loan_payment.amount, new_amount)
+
+        loan_payment.amount = new_amount
+        loan_payment.is_cash = is_cash
+        loan_payment.bank_account_id = new_bank_account_id
+
+        db.session.commit()
+
+        update_loan_is_active(loan)
+        
+        return jsonify({'message': 'Loan payment created successfully'}), 201
+
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        raise e
+    except Exception as e:
+        db.session.rollback()
+        raise e
 
 def update_loan_is_active(loan):
     db.session.refresh(loan)  # <-- refresh from DB

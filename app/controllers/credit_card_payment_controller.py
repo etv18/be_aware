e37861c196b9653e7dcf93bb622fca3428f8ce67
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-12-09 16:55:52 -0400

    handler to update credit card payments records

--- app/controllers/credit_card_payment_controller.py
+++ app/controllers/credit_card_payment_controller.py
@@ -2,6 +2,7 @@ from flask import request, jsonify
 from sqlalchemy.exc import SQLAlchemyError
 
 from decimal import Decimal
+import traceback
 
 from app.extensions import db
 from app.models.credit_card_payment import CreditCardPayment
@@ -38,6 +39,34 @@ def create_credit_card_payment():
         db.session.rollback()
         raise e
 
+def update_credit_card_payment(id):
+    try:
+        payment = CreditCardPayment.query.get(id)
+        if not payment: raise CreditCardPaymentDoesNotExists('This credit card payment was not found.')
+
+        amount = Decimal(request.form.get('amount'))
+        bank_account_id = request.form.get('select-bank-account')
+
+        if(amount <= 0): raise AmountIsLessThanOrEqualsToZero('Introduce a number bigger than 0')
+
+        bank_account = BankAccount.query.get(bank_account_id)
+        if not bank_account: raise BankAccountDoesNotExists('You need to select a bank account.')
+        
+        h_update_bank_account_money_on_update(
+            payment.bank_account_id, 
+            bank_account_id,
+            payment.amount,
+            amount
+        )
+
+        payment.amount=amount
+        payment.bank_account_id=bank_account_id
+
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        raise e
+
 #HELPERS
 def h_update_bank_account_money_on_create(id, amount):
     bank_account = BankAccount.query.get(id)
@@ -50,3 +79,31 @@ def h_update_bank_account_money_on_create(id, amount):
         raise AmountGreaterThanAvailableMoney('Insufficient founds.')
     
     bank_account.amount_available -= amount
+
+def h_update_bank_account_money_on_update(old_ba_id, new_ba_id, old_amount, new_amount):
+    if (old_ba_id == new_ba_id) and (old_amount == new_amount): return
+
+    old_bank_account = None
+    new_bank_account = None
+
+    try:
+        if old_ba_id: #if the bank account id is true it means it's bank account to bank account transaction
+            old_bank_account = BankAccount.query.get(old_ba_id)
+            if not old_bank_account:
+                raise BankAccountDoesNotExists('Bank account does not exists.')   
+            old_bank_account.amount_available += old_amount; #refund the money used to the previous bank account
+        
+        new_bank_account = BankAccount.query.get(new_ba_id)
+
+        if not new_bank_account:
+            raise BankAccountDoesNotExists('Bank account does not exists.')
+
+        if new_bank_account.amount_available <= 0:
+            raise NoAvailableMoney('Bank Account does not have any money left.')
+        if new_bank_account.amount_available < new_amount:
+            raise AmountGreaterThanAvailableMoney('Insufficient founds.')
+        
+        new_bank_account.amount_available -= new_amount;
+    except Exception as e:
+        traceback.print_exc()
+        raise e

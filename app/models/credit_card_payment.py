0aee5912e1003292cff96274d585e90866df7e70
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-12-09 11:43:29 -0400

    model to register payments to credit card

---
+++ app/models/credit_card_payment.py
@@ -0,0 +1,15 @@
+from app.extensions import db
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+class CreditCardPayment(db.Model):
+    __tablename__ = 'credit_card_payments'
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Numeric(10,2), nullable=False)
+    
+    credit_card_id = db.Column(db.Integer, db.ForeignKey('credit_cards.id'), nullable=False)
+    
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
+
+    credit_card = relationship('CreditCard', back_populates='payments')

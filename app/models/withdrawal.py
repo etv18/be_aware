99f44f119a0fa83ff27b8dc6db931e11bd79a686
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-12-10 09:21:39 -0400

    withdrawal model created

---
+++ app/models/withdrawal.py
@@ -0,0 +1,16 @@
+from app.extensions import db
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+class Withdrawal(db.Model):
+    __tablename__ = 'withdrawals'
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Numeric(10,2), nullable=False, default=0.00)
+    description = db.Column(db.String(255))
+
+    bank_account_id = db.Column(db.Integer, db.ForeignKey('bank_accounts.id'))
+
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
+
+    bank_account = relationship('BankAccount', back_populates='withdrawals')

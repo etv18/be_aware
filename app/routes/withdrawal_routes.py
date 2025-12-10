c3563b05b8c55ebd9d3e0c8b27398d1a1309ed43
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-12-10 11:44:02 -0400

    endpoint for creating withdrawal records

--- app/routes/withdrawal_routes.py
+++ app/routes/withdrawal_routes.py
@@ -1,13 +1,26 @@
 from flask import Blueprint, jsonify, request, render_template
 
+from app.controllers import withdrawal_controller
 from app.models.withdrawal import Withdrawal
+from app.models.bank_account import BankAccount
 
 withdrawal_bp = Blueprint('withdrawal', __name__, url_prefix='/withdrawals')
 
 @withdrawal_bp.route('index', methods=['GET'])
 def index():
-    #withdrawals = Withdrawal.query.all()
+    withdrawals = Withdrawal.query.all()
+    bank_accounts = BankAccount.query.all()
     context = {
-        #'withdrawals': withdrawals,
+        'withdrawals': withdrawals,
+        'bank_accounts': bank_accounts,
     }
-    return render_template('withdrawals/index.html')
+    return render_template('withdrawals/index.html', **context)
+
+@withdrawal_bp.route('/create', methods=['POST'])
+def create():
+    try:
+        withdrawal_controller.create_withdrawal()
+        return jsonify({'message': 'Withdrawal created successfully'}), 200
+    except Exception as e:
+        print(e)
+        return jsonify({'error': str(e)}), 400

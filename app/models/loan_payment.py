8d353752cf2574a38494f27afb626fcd11d0fe1a
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-12-10 17:03:15 -0400

    added function to assign code to each loan payment record

--- app/models/loan_payment.py
+++ app/models/loan_payment.py
@@ -1,6 +1,10 @@
 from app.extensions import db
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
+from sqlalchemy import event
+
+from app.utils.code_generator import generate_montly_sequence
+from app.utils import prefixes
 
 class LoanPayment(db.Model):
     __tablename__ = 'loan_payment'
@@ -10,9 +14,17 @@ class LoanPayment(db.Model):
     
     loan_id = db.Column(db.Integer, db.ForeignKey('loans.id'))
     bank_account_id = db.Column(db.Integer, db.ForeignKey('bank_accounts.id', name='bank_account_id'))
+    code = db.Column(db.String(50), nullable=False, server_default='TEMP')
 
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
     updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())  
 
     loan = relationship('Loan', back_populates='loan_payments')
     bank_account = relationship('BankAccount', back_populates='loan_payments')
+
+    @event.listens_for(db.session, 'before_flush')
+    def assign_code(session, flush_context, instances=None):
+        for obj in session.new:
+            if isinstance(obj, LoanPayment):
+                if not obj.code:
+                    obj.code = generate_montly_sequence(prefixes.LOAN_PAYMENT, LoanPayment)

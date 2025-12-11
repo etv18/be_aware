a8fad4abe9e3d10368fb2ce4abced6d3d3c1fdeb
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-12-11 16:47:50 -0400

    adapted function for deleting elements

--- app/static/scripts/utils/utils.js
+++ app/static/scripts/utils/utils.js
@@ -1,34 +1,32 @@
-const btnDeleteList = document.querySelectorAll('a.btn-danger');
+document.addEventListener('click', async e => {
+    const btn = e.target.closest('a.btn-danger');
+    if(!btn) return;
 
-btnDeleteList.forEach(btn => {
-    btn.addEventListener('click', function (e) {
-        e.preventDefault(); // stop the default action
-        
-        const deleteUrl = btn.getAttribute('href');
-        console.log(deleteUrl)
-        Swal.fire({
-            title: "Are you sure?",
-            text: "This record will be deleted.",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#d33",
-            cancelButtonColor: "#3085d6",
-            confirmButtonText: "Yes, delete it!"
-        }).then(async (result) => {
-            if (result.isConfirmed) {
+    e.preventDefault();
 
-                res = await fetch(`${deleteUrl}`, {
-                    method: 'DELETE'
-                });
-                console.log(res.json());
-                
-                if (res.ok) {
-                    location.reload();
-                } else {
-                    Swal.fire("Error", "Could not delete record", "error");
-                }
+    const deleteUrl = btn.getAttribute('href');
+
+    Swal.fire({
+        title: "Are you sure?",
+        text: "This record will be deleted.",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#d33",
+        cancelButtonColor: "#3085d6",
+        confirmButtonText: "Yes, delete it!"
+    }).then(async (result) => {
+        if (result.isConfirmed) {
+            res = await fetch(deleteUrl, {
+                method: 'DELETE'
+            });
+            console.log(res.json());
+            
+            if (res.ok) {
+                location.reload();
+            } else {
+                Swal.fire("Error", "Could not delete record", "error");
             }
-        });
+        }
     });
 });
 

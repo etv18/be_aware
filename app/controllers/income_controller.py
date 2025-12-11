2812d7d7df920ea3ac2328001d425dfacef4e0ba
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-12-10 21:30:34 -0400

    bug fixed so when you enter a string value which is not possible to turn it into a Decimal

--- app/controllers/income_controller.py
+++ app/controllers/income_controller.py
@@ -1,16 +1,17 @@
 from flask import request
 from sqlalchemy.exc import SQLAlchemyError
 
-from decimal import Decimal
+from decimal import Decimal, ConversionSyntax
 
 from app.models.income import Income
 from app.models.bank_account import BankAccount
-from app.exceptions.bankProductsException import BankAccountDoesNotExists, AmountIsLessThanOrEqualsToZero
+from app.exceptions.bankProductsException import BankAccountDoesNotExists, AmountIsLessThanOrEqualsToZero, NoBankProductSelected
 from app.extensions import db
+from app.utils.is_numeric_type import is_numeric_type
 
 def create_income():
-        try:    
-            amount = Decimal(request.form['amount'])
+        try:
+            amount = Decimal(request.form['amount']) if is_numeric_type(request.form['amount']) else Decimal('0')
             is_cash = request.form.get('is-cash') == 'on'
             bank_account_id = None
 
@@ -18,6 +19,8 @@ def create_income():
 
             if not is_cash:
                 bank_account_id = int(request.form.get('select-bank-account'))
+                if not bank_account_id or bank_account_id == 'none':
+                    return NoBankProductSelected('You must select a bank account')
                 update_bank_account_money_on_create(bank_account_id, amount)
                 
             income = Income(
@@ -41,17 +44,21 @@ def create_income():
 
 def update_income(income):
     try:
-        amount = Decimal(request.form['amount'])
+        amount = Decimal(request.form['amount']) if is_numeric_type(request.form['amount']) else Decimal('0')
         is_cash = request.form.get('is-cash') == 'on'
         new_bank_account_id = None
         selected_bank_account = request.form.get('select-bank-account')
-
+        
         if(amount <= 0): raise AmountIsLessThanOrEqualsToZero('Introduce a number bigger than 0')
 
-        if not is_cash and (selected_bank_account != None or selected_bank_account != '' or selected_bank_account != 'none'):
+        if not is_cash:
+            if not selected_bank_account or selected_bank_account == 'none': raise NoBankProductSelected('You must select a bank account')
             new_bank_account_id = int(selected_bank_account)
             new_bank_account = BankAccount.query.get(new_bank_account_id)
+            if not new_bank_account: raise NoBankProductSelected('You must select a bank account')
             update_bank_account_money_on_update(income.bank_account, new_bank_account, income.amount, amount)
+        else:
+            income.bank_account.amount_available -= income.amount
 
         income.amount = amount
         income.is_cash = is_cash
@@ -71,7 +78,8 @@ def update_income(income):
 
 def delete_income(income):
     try:
-        update_bank_account_money_on_delete(income.bank_account, income.amount)
+        if income.bank_account:
+            update_bank_account_money_on_delete(income.bank_account, income.amount)
         db.session.delete(income)
         db.session.commit()
     except SQLAlchemyError as e:
@@ -92,13 +100,14 @@ def update_bank_account_money_on_create(id, amount):
     bank_account.amount_available += amount
 
 def update_bank_account_money_on_update(old_bank_account, new_bank_account, old_amount, new_amount):
+
     if not new_bank_account:
         raise BankAccountDoesNotExists('The new bank account does not exists.')
     
     if new_amount <= 0:
         raise AmountIsLessThanOrEqualsToZero('You need to enter an amount greater than 0')
-    
-    old_bank_account.amount_available -= old_amount #Subtract the old amount from the previous bank account
+    if old_bank_account:
+        old_bank_account.amount_available -= old_amount #Subtract the old amount from the previous bank account
     new_bank_account.amount_available += new_amount
 
 def update_bank_account_money_on_delete(bank_account, amount):

890ef1b53ec8644413b1f74759c6bfda64c7cb73
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-12-11 09:13:50 -0400

    function which returns a tuple of all already created prefixes

--- app/utils/prefixes.py
+++ app/utils/prefixes.py
@@ -3,3 +3,12 @@ LOAN = 'LOAN'
 LOAN_PAYMENT = 'LOAN_P'
 INCOME = 'INC'
 EXPENSE = 'EXP'
+
+def get_all():
+    return (
+        WITHDRAWAL,
+        LOAN,
+        LOAN_PAYMENT,
+        INCOME,
+        EXPENSE
+    )

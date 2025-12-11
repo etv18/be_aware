bfeb8ff0da82245f485f6b8e10381a0efb53e90d
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-12-10 21:29:43 -0400

    function to add code when an income is created

--- app/models/income.py
+++ app/models/income.py
@@ -1,6 +1,10 @@
 from app.extensions import db
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
+from sqlalchemy import event
+
+from app.utils.code_generator import generate_montly_sequence
+from app.utils import prefixes
 
 class Income(db.Model):
     __tablename__ = 'incomes'
@@ -8,6 +12,7 @@ class Income(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     amount = db.Column(db.Numeric(10,2), nullable=False, default=0.00)
     is_cash = db.Column(db.Boolean, nullable=False, default=False)
+    code = db.Column(db.String(50), nullable=False, server_default='TEMP')
 
     bank_account_id = db.Column(db.Integer, db.ForeignKey('bank_accounts.id'))
 
@@ -15,3 +20,10 @@ class Income(db.Model):
     updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
 
     bank_account = relationship('BankAccount', back_populates='incomes')
+
+    @event.listens_for(db.session, 'before_flush')
+    def assign_code(session, flush_context, instances=None):
+        for obj in session.new:
+            if isinstance(obj, Income):
+                if not obj.code:
+                    obj.code = generate_montly_sequence(prefixes.INCOME, Income)

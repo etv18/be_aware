b23f9d85373b38be9d96884c8536cab928cf20b7
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-12-11 09:59:58 -0400

    new file transactional_classes

---
+++ app/utils/transactional_classes.py
@@ -0,0 +1,14 @@
+from app.models.expense import Expense
+from app.models.income import Income
+from app.models.loan_payment import LoanPayment
+from app.models.loan import Loan
+from app.models.withdrawal import Withdrawal
+
+def get_transactional_classes():
+    return (
+        Withdrawal,
+        Loan,
+        LoanPayment,
+        Income,
+        Expense
+    )

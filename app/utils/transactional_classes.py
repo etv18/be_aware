c83eadf2f3fbdf6fecd369bc944f11359ef2c878
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-12-11 10:01:21 -0400

    changed function name

--- app/utils/transactional_classes.py
+++ app/utils/transactional_classes.py
@@ -4,7 +4,7 @@ from app.models.loan_payment import LoanPayment
 from app.models.loan import Loan
 from app.models.withdrawal import Withdrawal
 
-def get_transactional_classes():
+def get_all():
     return (
         Withdrawal,
         Loan,

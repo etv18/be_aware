61ea1ff71f7d979157c52bfe32b2eb3e612ab750
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-12-15 20:19:39 -0400

    change " to '

--- app/models/withdrawal.py
+++ app/models/withdrawal.py
@@ -22,14 +22,14 @@ class Withdrawal(db.Model):
     
     def to_dict(self):
         return {
-            "id": self.id,
-            "amount": float(self.amount) if self.amount is not None else 0.0,
-            "description": self.description,
-            "code": self.code,
-            "bank_account_id": self.bank_account_id,
-            "bank_account_nick_name": self.bank_account.nick_name if self.bank_account else '-',
-            "created_at": self.created_at.isoformat() if self.created_at else None,
-            "updated_at": self.updated_at.isoformat() if self.updated_at else None,
+            'id': self.id,
+            'amount': float(self.amount) if self.amount is not None else 0.0,
+            'description': self.description,
+            'code': self.code,
+            'bank_account_id': self.bank_account_id,
+            'bank_account_nick_name': self.bank_account.nick_name if self.bank_account else '-',
+            'created_at': self.created_at.isoformat() if self.created_at else None,
+            'updated_at': self.updated_at.isoformat() if self.updated_at else None,
         }
 
     @event.listens_for(db.session, 'before_flush')

97cfe25ea6362f518ff43fe0f2dadfcd8a7f49bc
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-12-16 20:44:13 -0400

    cash ledger blueprint registered

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,19 @@ from logging.handlers import RotatingFileHandler
 
 from app.config import Config
 from app.extensions import db
-from app.routes import home_routes, bank_routes, bank_account_routes, credit_card_routes, expense_category_routes, expense_routes, income_routes, accounts_receivable_routes, credit_card_payment_routes, withdrawal_routes
+from app.routes import (
+    home_routes, 
+    bank_routes, 
+    bank_account_routes, 
+    credit_card_routes, 
+    expense_category_routes, 
+    expense_routes, 
+    income_routes, 
+    accounts_receivable_routes, 
+    credit_card_payment_routes, 
+    withdrawal_routes, 
+    cashledger_routes
+)
 
 def create_app():
     app = Flask(__name__)
@@ -37,6 +49,7 @@ def register_resources(app):
     app.register_blueprint(accounts_receivable_routes.accounts_receivable_bp)
     app.register_blueprint(credit_card_payment_routes.credit_card_payment_bp)
     app.register_blueprint(withdrawal_routes.withdrawal_bp)
+    app.register_blueprint(cashledger_routes.cashlegder_bp)
     
 def create_error_logger(app):
     if not os.path.exists('logs'):

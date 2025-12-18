62600eb5b06abc60746a024132df54e666780cdd
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-12-18 10:48:49 -0400

    new function to get the total amount of a given list

--- app/utils/numeric_casting.py
+++ app/utils/numeric_casting.py
@@ -6,3 +6,9 @@ def is_decimal_type(value: str) -> bool:
         return True
     except (InvalidOperation, ValueError):
         return False
+
+def total_amount(obj_list):
+        total = Decimal()
+        for o in obj_list:
+            total += o.amount
+        return f'DOP {total:,.2f}'

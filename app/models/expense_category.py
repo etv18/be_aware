e25718fb764f89565b6b1aa8a9df83401bfbd2be
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-12-18 15:32:47 -0400

    new field limit added to ExpenseCategory model

--- app/models/expense_category.py
+++ app/models/expense_category.py
@@ -7,7 +7,8 @@ class ExpenseCategory(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    limit = db.Column(db.Numeric(10,2))
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
     updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
 
-    expenses = relationship('Expense', back_populates='expense_category')
+    expenses = relationship('Expense', back_populates='expense_category', order_by='desc(Expense.created_at)')

9e6794702756a434c4c7f90dd70d41d7539781be
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-12-19 14:20:55 -0400

    default value of 0 to limit

--- app/models/expense_category.py
+++ app/models/expense_category.py
@@ -2,12 +2,14 @@ from app.extensions import db
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 
+from decimal import Decimal
+
 class ExpenseCategory(db.Model):
     __tablename__ = 'expense_categories'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    limit = db.Column(db.Numeric(10,2))
+    limit = db.Column(db.Numeric(10,2), server_default='0')
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
     updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
 

7ca5cfc96fa9a0d641cfcc9a90a8afd133c3900a
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-12-23 14:33:15 -0400

    refactor creditCardPaymentsTemplateFn

--- app/static/scripts/credit_cards/filterAssociatedRecords.js
+++ app/static/scripts/credit_cards/filterAssociatedRecords.js
@@ -1,11 +1,37 @@
 import { getTotalSumOfAmounts, formatNumber } from "../utils/numericHandling.js";
 import { debounce } from "../utils/asyncHanlding.js";
-import {renderDataTable, filterTableData, colSpan, creditCardPaymentsTemplateFn, expensesTemplateFn} from '../utils/dynamicTables.js'
+import {renderDataTable, filterTableData, colSpan, expensesTemplateFn} from '../utils/dynamicTables.js'
 
 const expenseFilterInput = document.getElementById('filter-input-expense-id');
 const creditCardPaymentFilterInput = document.getElementById('filter-input-credit-card-payment-id');
 const tBodyCreditCardPayment = document.getElementById('credit-card-payment-table-body');
 const tBodyExpense = document.getElementById('expenses-table-body');
+
+const creditCardPaymentsTemplateFn = (creditCardPayment) => `
+    <th scope="row">${ creditCardPayment.id }</th>
+    <td class="text-start">${ creditCardPayment.amount }</td>
+    <td>${ creditCardPayment.credit_card_nick_name ?? '-' }</td>
+    <td>${ creditCardPayment.bank_account_nick_name ?? '-' }</td>
+    <td>${ creditCardPayment.created_at }</td>
+    <td class="d-flex justify-content-center">
+        <div class="d-flex gap-2">
+            <button 
+                type="button" 
+                class="btn btn-success" 
+                data-bs-toggle="modal" 
+                data-bs-target="#edit-credit-card-payment"
+                data-endpoint="/credit_card_payments/update/${creditCardPayment.id}"
+                data-credit-card-payment-id="${ creditCardPayment.id }"
+                data-amount="${ creditCardPayment.amount }"
+                data-credit-card-id="${ creditCardPayment.credit_card_id }"
+                data-bank-account-id="${ creditCardPayment.bank_account_id }"
+            >
+            Edit
+            </button>
+            <a href="/credit_card_payments/delete/${ creditCardPayment.id }" class="btn btn-danger">Delete</a>
+        </div>
+    </td>
+`;
 
 let data;
 

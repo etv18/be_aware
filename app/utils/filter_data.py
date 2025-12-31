d3e0d5f599da8219a59e17e7b70e459224a9653e
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-12-30 20:28:04 -0400

    sort results by date in desc order

--- app/utils/filter_data.py
+++ app/utils/filter_data.py
@@ -3,9 +3,14 @@ from sqlalchemy import extract, func
 
 def get_monthly_records(CustomModel, year, month):
     
-    records = CustomModel.query.filter(
-        extract('year', CustomModel.created_at) == year,
-        extract('month', CustomModel.created_at) == month
-    ).all()
-
+    records = ( 
+        CustomModel.query
+        .filter(
+            extract('year', CustomModel.created_at) == year,
+            extract('month', CustomModel.created_at) == month
+        )
+        .order_by(CustomModel.created_at.desc())
+        .all()
+    )
     return records
+

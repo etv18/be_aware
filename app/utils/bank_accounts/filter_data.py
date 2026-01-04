a2f44c1515a7edd735e9bd3449182fb93c8627bc
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-01-04 14:48:51 -0400

    created function to get outgoings and incoming transfers yearly and monthly reports of bank account

--- app/utils/bank_accounts/filter_data.py
+++ app/utils/bank_accounts/filter_data.py
@@ -4,7 +4,10 @@ from sqlalchemy import func
 from decimal import Decimal
 
 from app.extensions import db
+from app.models.banktransfer import BankTransfer
 
+
+''' MONTHLY REPORTS '''
 def get_monthly_records(id, CustomModel, year, month) -> list:
     try:
         records = ( 
@@ -41,6 +44,44 @@ def get_monthly_total_amount_info(id, CustomModel, year, month) -> list:
         traceback.print_exc()
         raise e
 
+
+def get_monthly_total_amount_info_of_transfers(id, year, month):
+    try:
+        outgoings_total_amount = ( 
+            BankTransfer.query
+            .filter(
+                BankTransfer.from_bank_account_id == id,
+                func.extract('year', BankTransfer.created_at) == year,
+                func.extract('month', BankTransfer.created_at) == month
+            )
+            .with_entities(func.sum(BankTransfer.amount))
+            .scalar() or Decimal(0.00)
+        )
+        
+        incomings_total_amount = ( 
+            BankTransfer.query
+            .filter(
+                BankTransfer.to_bank_account_id == id,
+                func.extract('year', BankTransfer.created_at) == year,
+                func.extract('month', BankTransfer.created_at) == month
+            )
+            .with_entities(func.sum(BankTransfer.amount))
+            .scalar() or Decimal(0.00)
+        )
+
+        data = {
+            'outgoings': outgoings_total_amount,
+            'incomings': incomings_total_amount
+        }
+
+        return data
+        
+    except Exception as e:
+        db.session.rollback()
+        traceback.print_exc()
+        raise e 
+
+''' YEARLY REPORTS '''
 def get_yearly_records(id, CustomModel, year=None) -> list:
     if year is None:
         year = datetime.now().year
@@ -74,3 +115,27 @@ def get_yearly_total_amount_info(id, CustomModel, year=None) -> list:
         monthly_totals.append(record)
     
     return monthly_totals
+
+def get_yearly_total_amount_info_of_transfers(id, year=None) -> list:
+    if year is None:
+        year = datetime.now().year
+
+    outgoings = []
+    incomings = []
+
+    for month in range(1, 13):
+        results = {} #Dictionary
+        results = get_monthly_total_amount_info_of_transfers(
+            id=id,
+            year=year,
+            month=month
+        )
+        outgoings.append(results['outgoings'])
+        incomings.append(results['incomings'])
+
+    transfers = {
+        'outgoings': outgoings,
+        'incomings': incomings,
+    }
+
+    return transfers

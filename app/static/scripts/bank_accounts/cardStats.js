03a35267c60781c6028b76933ad370a9e90adbdb
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-01-09 00:18:34 -0400

    Add stat card styles and update stats template for monthly totals display

---
+++ app/static/scripts/bank_accounts/cardStats.js
@@ -0,0 +1,49 @@
+const endpoint = document.getElementById('total-monthly-per-associated-record-info-endpoint-id').value;
+
+const expensesTotal = document.getElementById('monthly-expenses-total-id');
+const incomesTotal = document.getElementById('monthly-incomes-total-id');
+const loanPaymentTotal = document.getElementById('monthly-loan-payment-total-id');
+const loansTotal = document.getElementById('monthly-loans-total-id');
+const creditCardPaymentsTotal = document.getElementById('monthly-credit-card-payments-total-id');
+const transfersOutgoingsTotal = document.getElementById('monthly-transfers-outgoings-total-id');
+const transfersIncomingsTotal = document.getElementById('monthly-transfers-incomings-total-id');
+const withdrawalsTotal = document.getElementById('monthly-withdrawals-total-id');
+
+async function getMonthlyTotalPerAssociatedRecord(){
+    try {
+        response = await fetch(endpoint);
+        if(!response.ok) {
+            console.log(response.json());
+            return;
+        }
+
+        const data = await response.json();
+        
+        return data;
+    } catch (error) {
+        console.error(error);
+        throw new Error();
+    }
+}
+
+function formatNumber(value){
+    const numberValue = Number(value || 0);
+    
+    return '$'+numberValue.toLocaleString('en-US', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+}
+
+document.addEventListener('DOMContentLoaded', async e => {
+    const data = await getMonthlyTotalPerAssociatedRecord();
+
+    expensesTotal.textContent = formatNumber(data.monthly_totals.expenses);
+    incomesTotal.textContent = formatNumber(data.monthly_totals.incomes);
+    loanPaymentTotal.textContent = formatNumber(data.monthly_totals.loan_payment);
+    loansTotal.textContent = formatNumber(data.monthly_totals.loans);
+    creditCardPaymentsTotal.textContent = formatNumber(data.monthly_totals.credit_card_payments);
+    transfersOutgoingsTotal.textContent = formatNumber(data.monthly_totals.transfers_outgoings);
+    transfersIncomingsTotal.textContent = formatNumber(data.monthly_totals.transfers_incomings);
+    withdrawalsTotal.textContent = formatNumber(data.monthly_totals.withdrawals);
+});

be3d7ddf58a3860d1ddffa410ea2bc790970d58c
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-01-14 17:01:24 -0400

    refactor index route to return ledgers with formatted amounts

--- app/routes/bank_transaction_ledger_routes.py
+++ app/routes/bank_transaction_ledger_routes.py
@@ -2,9 +2,27 @@ from flask import Blueprint, jsonify, request, render_template
 
 from app.models.bank_account_transactions_ledger import BankAccountTransactionsLedger
 from app.controllers import bank_transaction_ledger_controller as controller
+from app.utils.numeric_casting import format_amount
 
 bank_transaction_ledger_bp = Blueprint('bank_transaction_ledger', __name__, url_prefix='/bank_transaction_ledger')
 
 @bank_transaction_ledger_bp.route('/index')
 def index():
-    render_template('/bank_transaction_ledger/index.html')
+    ledgers = (
+        BankAccountTransactionsLedger.query
+        .order_by(BankAccountTransactionsLedger.created_at.desc())
+        .all()
+    )
+    context = {
+        'ledgers': ledgers,
+        'format_amount': format_amount,
+    }
+    return render_template('bank_transaction_ledger/index.html', **context)
+
+@bank_transaction_ledger_bp.route('/filter/by/field')
+def filter_by_field():
+    try:
+        query = request.args.get('query')
+        return controller.filter_by_field(query) 
+    except Exception as e:
+        raise e

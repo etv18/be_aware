e154d5331bc18884049bd83db6600b63ae4d6786
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-01-15 16:36:46 -0400

    debt_payment routes file created

---
+++ app/routes/debt_payment_routes.py
@@ -0,0 +1,39 @@
+from flask import Blueprint, redirect, render_template, request, url_for, jsonify
+from sqlalchemy import func
+
+from app.controllers import debt_payment_controller as controller
+from app.models.bank_account import BankAccount
+from app.utils.numeric_casting import format_amount, total_amount
+
+debt_payment_bp = Blueprint('debt_payment', __name__, url_prefix='/debt_payments')
+
+@debt_payment_bp.route('/index')
+def index():
+    bank_accounts = BankAccount.query.all()
+    context = {
+        'bank_accounts': bank_accounts,
+        'format_amount': format_amount,
+        'total_amount': total_amount,
+    }
+    return render_template('debts/index.html', **context)
+
+@debt_payment_bp.route('/create', methods=['POST'])
+def create():
+    try:
+        return controller.create()
+    except Exception as e:
+        raise e
+
+@debt_payment_bp.route('/update/<int:id>', methods=['PUT'])
+def update(id):
+    try:
+        return controller.update(id)
+    except Exception as e:
+        raise e
+
+@debt_payment_bp.route('/delete/<int:id>', methods=['DELETE'])
+def delete(id):
+    try:
+        return controller.delete(id)
+    except Exception as e:
+        raise e

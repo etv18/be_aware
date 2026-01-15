3cde6d12f76553d0d9932c5eb8ead992f31ad196
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-01-15 12:00:16 -0400

    passed all debts record in context for index template

--- app/routes/debt_routes.py
+++ app/routes/debt_routes.py
@@ -1,12 +1,15 @@
 from flask import Blueprint, redirect, render_template, request, url_for, jsonify
 from sqlalchemy import func
 
+from app.models.debt import Debt
 from app.controllers import debt_controller as controller
 
 debt_bp = Blueprint('debt', __name__, url_prefix='/debts')
 
 @debt_bp.route('/index')
 def index():
+    debts = Debt.query.all()
     context = {
+        'debts': debts,
     }
     render_template('debts/index.html', **context)

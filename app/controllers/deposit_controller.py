e445d79f695bbbd3b37150f899c45935faed575c
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-01-16 15:32:45 -0400

    refactor delete method to handle deposits and update bank account balance accordingly

--- app/controllers/deposit_controller.py
+++ app/controllers/deposit_controller.py
@@ -90,24 +90,30 @@ def update(id):
         db.session.rollback()
         traceback.print_exc()
         return jsonify({'error': str(e)}), 400
-'''
+
 def delete(id):
     try:
-        withdrawal = Withdrawal.query.get(id)
-        if not withdrawal: raise NotRecordFound('No withdrawal was found')
+        deposit = Deposit.query.get(id)
+        if not deposit: raise NotRecordFound('No deposit was found')
 
-        withdrawal.bank_account.amount_available += withdrawal.amount #return money used from the selected bank account
 
-        db.session.delete(withdrawal)
+        _update_bank_account_money_on_delete(
+            bank_account=deposit.bank_account,
+            amount=deposit.amount
+        )
+
+        db.session.delete(deposit)
         db.session.commit()
 
-        CashLedger.update_or_delete(withdrawal, delete_ledger=True)
-        BankAccountTransactionsLedger.delete(withdrawal)
+        CashLedger.update_or_delete(deposit, delete_ledger=True)
+        BankAccountTransactionsLedger.delete(deposit)
+
+        return jsonify({'message': 'deposit deleted successfully'}), 200
     except Exception as e:
         db.session.rollback()
         traceback.print_exc()
-        raise e
-'''
+        return jsonify({'error': str(e)}), 400
+
     
 def filter_withdrawals_by_field(query):
     try:
@@ -172,6 +178,14 @@ def _update_bank_account_money_on_create(bank_account, amount):
     
     bank_account.amount_available += amount
 
+def _update_bank_account_money_on_delete(bank_account, amount):
+    if not bank_account: return
+
+    if bank_account.amount_available < amount:
+        raise AmountGreaterThanAvailableMoney(f"Insufficient founds from '{bank_account.nick_name}' bank account to delete this deposit.")
+    
+    bank_account.amount_available -= amount
+
 
 def _update_bank_account_money_on_update(old_bank_account, new_bank_account, old_amount, new_amount):
     if (old_bank_account.id == new_bank_account.id) and (old_amount == new_amount): return
@@ -182,7 +196,7 @@ def _update_bank_account_money_on_update(old_bank_account, new_bank_account, old
         if old_bank_account.amount_available <= 0:
             raise NoAvailableMoney(f"'{old_bank_account.nick_name}' bank account does not have any money left.")
         if old_bank_account.amount_available < new_amount:
-            raise AmountGreaterThanAvailableMoney(f"Insufficient founds from '{old_bank_account.nick_name}' bank account.")
+            raise AmountGreaterThanAvailableMoney(f"Insufficient founds from '{old_bank_account.nick_name}' bank account to update this deposit.")
         
         old_bank_account.amount_available -= old_amount; #subtract the money used from previous bank account
         

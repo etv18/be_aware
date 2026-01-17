2e2c87a9b8434969207a5e903c5731c8cd36c4e2
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-01-17 11:48:29 -0400

    addapted js file to filter debt payments table

--- app/static/scripts/accounts_payable/filterAssociatedRecords.js
+++ app/static/scripts/accounts_payable/filterAssociatedRecords.js
@@ -2,33 +2,33 @@ import { getTotalSumOfAmounts, formatNumber } from "../utils/numericHandling.js"
 import { debounce } from "../utils/asyncHanlding.js";
 import {renderDataTable, filterTableData, colSpan } from '../utils/dynamicTables.js'
 
-const tBodyLoanPayment = document.getElementById('loan-payment-table-body');
-const loanPaymentFilterInput = document.getElementById('filter-input-loan-payment-id');
+const tBodyDebtPayment = document.getElementById('debt-payment-table-body');
+const debtPaymentFilterInput = document.getElementById('filter-input-debt-payment-id');
 let data;
 
-const loanPaymentsTemplateFn = (loanPayment) => `
-    <th scope="row">${ loanPayment.id }</th>
-    <td class="text-start">${ loanPayment.amount }</td>
-    <td>${ loanPayment.is_cash ? 'YES' : 'NO' }</td>
-    <td class="text-start">${ loanPayment.bank_account_nick_name ?? '-'}</td>
-    <td>${ loanPayment.created_at }</td>
+const debtPaymentsTemplateFn = (debtPayment) => `
+    <th scope="row">${ debtPayment.id }</th>
+    <td class="text-start">${ debtPayment.amount }</td>
+    <td>${ debtPayment.is_cash ? 'YES' : 'NO' }</td>
+    <td class="text-start">${ debtPayment.bank_account_nick_name ?? '-'}</td>
+    <td>${ debtPayment.created_at }</td>
     <td>
         <div class="d-flex gap-2 justify-content-center">
             <button 
                 type="button" 
                 class="btn btn-outline-success" 
                 data-bs-toggle="modal" 
-                data-bs-target="#edit-loan-payment"
-                data-endpoint="/accounts_receivable/update_loan_payment/${ loanPayment.id }"
-                data-loan-id="${ loanPayment.loan_id }"
-                data-loan-payment-id="${ loanPayment.id }"
-                data-amount="${ loanPayment.amount }"
-                data-is-cash="${ loanPayment.is_cash }"
-                data-bank-account-id="${ loanPayment.bank_account_id }"
+                data-bs-target="#edit-debt-payment"
+                data-endpoint="/debt_payments/update/${ debtPayment.id }"
+                data-debt-id="${ debtPayment.debt_id }"
+                data-debt-payment-id="${ debtPayment.id }"
+                data-amount="${ debtPayment.amount }"
+                data-is-cash="${ debtPayment.is_cash }"
+                data-bank-account-id="${ debtPayment.bank_account_id }"
             >
                 <i class="bi bi-pen"></i>
             </button>
-            <a href="/accounts_receivable/delete_loan_payment/${loanPayment.id}" class="btn btn-danger"><i class="bi bi-trash"></i></a>
+            <a href="/debt_payments/delete/${debtPayment.id}" class="btn btn-danger"><i class="bi bi-trash"></i></a>
         </div>
     </td>
 `;
@@ -54,13 +54,13 @@ document.addEventListener('DOMContentLoaded', async e => {
     data = await getAssociatedRecords();
 })
 
-loanPaymentFilterInput.addEventListener('input', debounce(async e => {
-        const totatAmounts = document.getElementById('total-amounts-loans-payments-id');
-        const totalCount = document.getElementById('total-count-loans-payments-id');
-        var query = loanPaymentFilterInput.value;
-        const filteredList = filterTableData(data.records.loan_payment, query);
+debtPaymentFilterInput.addEventListener('input', debounce(async e => {
+        const totatAmounts = document.getElementById('total-amounts-debts-payments-id');
+        const totalCount = document.getElementById('total-count-debts-payments-id');
+        var query = debtPaymentFilterInput.value;
+        const filteredList = filterTableData(data.records.debt_payments, query);
 
-        renderDataTable(filteredList, tBodyLoanPayment, loanPaymentsTemplateFn, colSpan.LOAN_PAYMENTS);
+        renderDataTable(filteredList, tBodyDebtPayment, debtPaymentsTemplateFn, colSpan.LOAN_PAYMENTS);
         totatAmounts.textContent = (`TOTAL: ${formatNumber(getTotalSumOfAmounts(filteredList))}`);
         totalCount.textContent = `COUNT: ${filteredList.length}`;
     })

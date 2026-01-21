ca667395f511ac6f1521413bd7405be87b39a492
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-01-20 23:29:23 -0400

    added new yearly chart to show a monthly total through the year of each bank association

--- app/static/scripts/bank_accounts/stats.js
+++ app/static/scripts/bank_accounts/stats.js
@@ -1,12 +1,14 @@
-const yearlyEndpoint = document.getElementById('yearly-cash-flow-endpoint-id').value;
-const yearlyCanvas = document.getElementById('yearly-stats');
+const yearlyCashFlowEndPoint = document.getElementById('yearly-cash-flow-endpoint-id').value;
+const yearlyTotalPerAssociationEndPoint = document.getElementById('yearly-total-per-association-endpoint-id').value;
+const yearlyCashFlowCanvas = document.getElementById('yearly-stats');
+const yearlyTotalPerAssociationCanvas = document.getElementById('yearly-total-per-association-chart');
 const tableCashFlow = document.getElementById('cash-flow-table-id');
 const theadCashFlow = tableCashFlow.querySelector('thead');
 const tbodyCashFlow = tableCashFlow.querySelector('tbody');
 
-async function getYearlyCashFlowData(){
+async function getData(endpoint){
     try {
-        response = await fetch(yearlyEndpoint);
+        response = await fetch(endpoint);
         if(!response.ok) {
             console.log(response.json());
             return;
@@ -21,7 +23,7 @@ async function getYearlyCashFlowData(){
     }
 }
 
-function generateYearlyChart(canvas, type, data){
+function generateYearlyCashFlowChart(canvas, type, data){
     try {
         new Chart(canvas, {
             type: type,
@@ -42,6 +44,109 @@ function generateYearlyChart(canvas, type, data){
                         borderColor: "rgba(75, 192, 192, 1)",
                         borderWidth: 1 
                     }
+                ]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        ticks: {stepSize: 500}
+                    }
+                }
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        throw new Error();
+    }
+}
+
+function generateYearlyTotalPerAssociationChart(canvas, type, data){
+    try {
+        new Chart(canvas, {
+            type: type,
+            data: {
+                labels: data.months,
+                datasets: [
+                    {
+                        label: 'Expenses',
+                        data: data.associations_info.expenses,
+                        backgroundColor: "rgba(255, 99, 132, 0.6)",
+                        borderColor: "rgba(255, 99, 132, 1)",
+                        borderWidth: 1                       
+                    },
+                    {
+                        label: 'Withdrawals',
+                        data: data.associations_info.withdrawals,
+                        backgroundColor: "rgba(75, 192, 192, 0.6)",
+                        borderColor: "rgba(75, 192, 192, 1)",
+                        borderWidth: 1 
+                    },
+                    {
+                        label: 'Loans',
+                        data: data.associations_info.loans,
+                        backgroundColor: "rgba(75, 192, 192, 0.6)",
+                        borderColor: "rgba(75, 192, 192, 1)",
+                        borderWidth: 1 
+                    },
+                    {
+                        label: 'Credit Card Payments',
+                        data: data.associations_info.credit_card_payments,
+                        backgroundColor: "rgba(75, 192, 192, 0.6)",
+                        borderColor: "rgba(75, 192, 192, 1)",
+                        borderWidth: 1 
+                    },
+                    {
+                        label: 'Debt Payments',
+                        data: data.associations_info.debt_payments,
+                        backgroundColor: "rgba(75, 192, 192, 0.6)",
+                        borderColor: "rgba(75, 192, 192, 1)",
+                        borderWidth: 1 
+                    },
+                    {
+                        label: 'Loan Payment',
+                        data: data.associations_info.loan_payment,
+                        backgroundColor: "rgba(75, 192, 192, 0.6)",
+                        borderColor: "rgba(75, 192, 192, 1)",
+                        borderWidth: 1 
+                    },
+                    {
+                        label: 'Incomes',
+                        data: data.associations_info.incomes,
+                        backgroundColor: "rgba(75, 192, 192, 0.6)",
+                        borderColor: "rgba(75, 192, 192, 1)",
+                        borderWidth: 1 
+                    },
+                    {
+                        label: 'Deposits',
+                        data: data.associations_info.deposits,
+                        backgroundColor: "rgba(75, 192, 192, 0.6)",
+                        borderColor: "rgba(75, 192, 192, 1)",
+                        borderWidth: 1 
+                    },
+                    {
+                        label: 'Debts',
+                        data: data.associations_info.debts,
+                        backgroundColor: "rgba(75, 192, 192, 0.6)",
+                        borderColor: "rgba(75, 192, 192, 1)",
+                        borderWidth: 1 
+                    },
+                    {
+                        label: 'Outgoing Transfers',
+                        data: data.associations_info.outgoing_transfers,
+                        backgroundColor: "rgba(75, 192, 192, 0.6)",
+                        borderColor: "rgba(75, 192, 192, 1)",
+                        borderWidth: 1 
+                    },
+                    {
+                        label: 'Incoming Transfers',
+                        data: data.associations_info.incoming_transfers,
+                        backgroundColor: "rgba(75, 192, 192, 0.6)",
+                        borderColor: "rgba(75, 192, 192, 1)",
+                        borderWidth: 1 
+                    },
                 ]
             },
             options: {
@@ -113,7 +218,10 @@ function buildCashFlowTable(data){
 }
 
 document.addEventListener('DOMContentLoaded', async e => {
-    var data = await getYearlyCashFlowData();
-    await generateYearlyChart(yearlyCanvas, 'bar', data);
+    var data = await getData(yearlyCashFlowEndPoint);
+    await generateYearlyCashFlowChart(yearlyCashFlowCanvas, 'bar', data);
     buildCashFlowTable(data);
+
+    data = await getData(yearlyTotalPerAssociationEndPoint);
+    await generateYearlyTotalPerAssociationChart(yearlyTotalPerAssociationCanvas, 'line', data)
 });

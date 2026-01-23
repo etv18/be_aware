43239627691a15767492339af8aedf356eea7879
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-01-23 17:57:44 -0400

    created routes file for credit card transactions

---
+++ app/routes/credit_card_transaction_routes.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, jsonify, request, render_template
+
+from app.models.credit_card_transactions_ledger import CreditCardTransactionsLedger
+from app.controllers import credit_card_transaction_controller as controller
+from app.utils.numeric_casting import format_amount
+
+credit_card_ledger_bp = Blueprint('credit_card_ledger', __name__, url_prefix='/credit_card_ledger')
+
+@credit_card_ledger_bp.route('/index')
+def index():
+    ledgers = (
+        CreditCardTransactionsLedger.query
+        .order_by(CreditCardTransactionsLedger.created_at.desc())
+        .all()
+    )
+    context = {
+        'ledgers': ledgers,
+        'format_amount': format_amount,
+    }
+    return render_template('credit_card_ledger/index.html', **context)
+
+@credit_card_ledger_bp.route('/filter/by/field')
+def filter_by_field():
+    try:
+        query = request.args.get('query')
+        return controller.filter_by_field(query) 
+    except Exception as e:
+        raise e
+
+@credit_card_ledger_bp.route('/filter/by/time')
+def filter_by_time():
+    try:
+        start = request.args.get('start')
+        end = request.args.get('end')
+        return controller.filter_by_time(start, end) 
+    except Exception as e:
+        raise e
+    

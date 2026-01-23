23577073411b7a0b3e28d53f1a8cb59fb5181720
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-01-23 15:48:28 -0400

    deleted unused variable

--- app/routes/debt_routes.py
+++ app/routes/debt_routes.py
@@ -12,7 +12,6 @@ debt_bp = Blueprint('debt', __name__, url_prefix='/debts')
 def index():
     debts = Debt.query.order_by(Debt.created_at.desc()).all()
     bank_accounts = BankAccount.query.all()
-    ramaining_to_pay = Debt.query.filter(Debt.is_active == True).with_entities(func.sum(Debt.amount)).scalar()
     actives_debts = Debt.query.filter(Debt.is_active == True).count()
     paids_debts = Debt.query.filter(Debt.is_active == False).count()
 

55f3739bf6649f0b0cf991fecf2052dffbc799f9
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-01-24 09:25:37 -0400

    added util function total_amount

--- app/routes/credit_card_transaction_routes.py
+++ app/routes/credit_card_transaction_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify, request, render_template
 
 from app.models.credit_card_transactions_ledger import CreditCardTransactionsLedger
 from app.controllers import credit_card_transaction_controller as controller
-from app.utils.numeric_casting import format_amount
+from app.utils.numeric_casting import format_amount, total_amount
 
 credit_card_ledger_bp = Blueprint('credit_card_ledger', __name__, url_prefix='/credit_card_ledger')
 
@@ -16,6 +16,7 @@ def index():
     context = {
         'ledgers': ledgers,
         'format_amount': format_amount,
+        'total_amount': total_amount,
     }
     return render_template('credit_cards/ledger.html', **context)
 

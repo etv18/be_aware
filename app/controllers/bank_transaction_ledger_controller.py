1029f946950ac293e367834ccc72a701b8c51ada
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-01-25 15:12:01 -0400

    filter all controller set up

--- app/controllers/bank_transaction_ledger_controller.py
+++ app/controllers/bank_transaction_ledger_controller.py
@@ -71,18 +71,49 @@ def filter_by_time(start, end):
         traceback.print_exc()
         raise e
     
-def filter_all(query, start, end):
+def filter_all():
     try:
-        if not query and (not start or not end):
-            return
+        data = request.get_json(silent=True) or {}
 
-        start_date = datetime.strptime(start, '%Y-%m-%d')
-        end_date = datetime.strptime(end, '%Y-%m-%d')
-        end_date += timedelta(days=1)
+        query = data.get('query')
+        start = data.get('start')
+        end = data.get('end')
+
+        if not query and (not start or not end):
+            return jsonify({
+                'error': 'Try to type some query or select a time frame.'
+            }), 400
+
+        and_filters = []
+
+        if start and end:
+            start_date = datetime.strptime(start, '%Y-%m-%d')
+            end_date = datetime.strptime(end, '%Y-%m-%d')
+            end_date += timedelta(days=1)
+            and_filters.append(BankAccountTransactionsLedger.created_at.between(start_date, end_date))
+
+        if query: 
+            q = f'%{query}%'
+
+        #            .outerjoin(Expense.bank_account) #allows to show expense without a bank account
+
+
+            text_filters = db.or_(
+                (BankAccountTransactionsLedger.amount.ilike(q)),
+                (BankAccountTransactionsLedger.before_update_balance.ilike(q)),
+                (BankAccountTransactionsLedger.after_update_balance.ilike(q)),
+                (BankAccountTransactionsLedger.reference_code.ilike(q)),
+                (BankAccountTransactionsLedger.transaction_type.ilike(q)),
+                (BankAccountTransactionsLedger.created_at.ilike(q)),
+                (BankAccount.nick_name.ilike(q))
+            )
+
+            and_filters.append(text_filters)
 
         ledgers = (
             BankAccountTransactionsLedger.query
-            .filter(BankAccountTransactionsLedger.created_at.between(start_date, end_date))
+            .outerjoin(BankAccountTransactionsLedger.bank_account)
+            .filter(db.and_(*and_filters))
             .order_by(BankAccountTransactionsLedger.created_at.desc())
             .all()
         )
@@ -98,4 +129,4 @@ def filter_all(query, start, end):
     except Exception as e:
         db.session.rollback()
         traceback.print_exc()
-        raise e
+        return jsonify({'error': 'Internal server error'}), 500

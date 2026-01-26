f44b451d478065e964c01d821b2904bcc9f7a60d
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-01-26 13:55:43 -0400

    refactor js file for filtering by input text and time frame

--- app/static/scripts/cashledger/filterData.js
+++ app/static/scripts/cashledger/filterData.js
@@ -2,14 +2,27 @@ import { debounce } from "../utils/asyncHanlding.js";
 import { titleCase } from "../utils/stringHandling.js";
 import { formatNumber } from "../utils/numericHandling.js";
 
-const selectFilterType = document.getElementById('select-filter-type-id');
-const filterInput = document.getElementById('filter-input-id');
+const filterByTimeInput = document.getElementById('filter-bytime-input-id');
 const btnSearch = document.getElementById('btn-search-id');
 const lblTotal = document.getElementById('lbl-total');
+const filterByFieldInput = document.getElementById('filter-byfiled-input-id');
+const filterDataEndpoint = document.getElementById('filter-data-endpoint').value;
 
-let timePicker = null;
 let startDate = null;
-let endDate = null; 
+let endDate = null;
+
+const timePicker = flatpickr(filterByTimeInput, {
+    mode: 'range',
+    altInput: true,
+    altFormat: 'M j, Y',
+    dateFormat: 'Y-m-d',
+    onChange: (selectedDates, dateStr, instance) => {
+        if(selectedDates.length === 2){
+            startDate = selectedDates[0];
+            endDate = selectedDates[1];
+        }
+    }
+});
 
 function renderDataTable(ledgers){
     const tbody = document.getElementById('cashledger-table-body-id');
@@ -29,7 +42,7 @@ function renderDataTable(ledgers){
 
         tableRow.innerHTML = `
             <th scope="row">${ ledger.id }</th>
-            <td>${ ledger.reference_code }</td>
+            <td class="text-start">${ ledger.reference_code }</td>
             <td class="text-start">${ titleCase(ledger.type) }</td>
             <td class="text-start">${ formatNumber(ledger.amount) }</td>
             <td>${ ledger.created_at }</td>
@@ -39,21 +52,31 @@ function renderDataTable(ledgers){
     });
 }
 
-async function getData(url){
+async function getData(url, payload) {
     let data;
 
     try {
-        const response = await fetch(url);
-        if(!response.ok) {
-            console.log(response.json());
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
             Swal.fire({
                 icon: 'error',
                 title: 'Error',
-                text: data.error || 'Something went wrong while creating the expense.'
+                text: errorData.error || 'Something went wrong.'
             });
             return;
         }
+
         data = await response.json();
+        console.log(data);
+
     } catch (error) {
         Swal.fire({
             icon: 'error',
@@ -66,64 +89,42 @@ async function getData(url){
 }
 
 async function filterData(){
-    let url = '';
+    let payload = {
+        query: null,
+        start: null,
+        end: null
+    };
 
-    if(selectFilterType.value === 'field'){
-        url = `/cashledger/filter/cashledger/by/field?query=${filterInput.value}`;
-    } else {
-        if(startDate === null || endDate === null){ 
-            Swal.fire({
-                icon: 'error',
-                title: 'Error',
-                text: 'You need to select two dates.'
-            });
-            return;
-        }  
-        const start = timePicker.formatDate(startDate, 'Y-m-d');      
-        const end = timePicker.formatDate(endDate, 'Y-m-d'); 
-
-        url = `/cashledger/filter/cashledger/by/time?start=${start}&end=${end}`;
+    if(filterByFieldInput.value.length > 0){
+        payload.query = filterByFieldInput.value?.trim() || null;
     }
 
-    const data = await getData(url);
+    payload.start = startDate ? timePicker.formatDate(startDate, 'Y-m-d') : null;
+    payload.end = endDate ? timePicker.formatDate(endDate, 'Y-m-d') : null;
+
+    const data = await getData(filterDataEndpoint, payload);
+
+    if (!data) return;
 
     renderDataTable(data.ledgers);
 
-    lblTotal.textContent = 'Available Cash: $'+ data.total;
+    lblTotal.textContent = 'Total: $'+ data.total;
 }
 
-selectFilterType.addEventListener('change', (e) => {
-    if(e.target.value === 'time'){
-        timePicker = flatpickr(filterInput, {
-            mode: 'range',
-            altInput: true,
-            altFormat: 'M j, Y',
-            dateFormat: 'Y-m-d',
-            onChange: (selectedDates, dateStr, instance) => {
-                if(selectedDates.length === 2){
-                    startDate = selectedDates[0];
-                    endDate = selectedDates[1];
-                }
-            }
-        });
-        return;
-    }
-
-    if(timePicker){
-        timePicker.destroy();
-        timePicker = null;
-    }
-
-    filterInput.type = "text";
-    filterInput.value = "";
-    
-});
-
 btnSearch.addEventListener('click', async e => {
+    if(!startDate || !endDate) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'You need to select two dates.'
+        });
+        return;    
+    }
     await filterData();
 });
 
-filterInput.addEventListener('keydown', debounce(async e => {
-    if(selectFilterType.value !== 'field' && e.key !== 'Enter') return;
+filterByFieldInput.addEventListener('keydown', debounce(async e => {
+    if(!filterByFieldInput.value.trim()) return;
+    console.log('filterByFieldInput => '+filterByFieldInput.value)
     await filterData();
 }));

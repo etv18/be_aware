357462f3b9b78eba76f0f3318483277b34a81d63
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-01-26 14:25:57 -0400

    created new controller for filtering credit card ledgers by input text and time frame at the same time

--- app/controllers/credit_card_transaction_controller.py
+++ app/controllers/credit_card_transaction_controller.py
@@ -70,3 +70,61 @@ def filter_by_time(start, end):
         db.session.rollback()
         traceback.print_exc()
         raise e
+    
+   
+def filter_all():
+    try:
+        data = request.get_json(silent=True) or {}
+
+        query = data.get('query')
+        start = data.get('start')
+        end = data.get('end')
+
+        if not query and (not start or not end):
+            return jsonify({
+                'error': 'Try to type some query or select a time frame.'
+            }), 400
+
+        and_filters = []
+
+        if start and end:
+            start_date = datetime.strptime(start, '%Y-%m-%d')
+            end_date = datetime.strptime(end, '%Y-%m-%d')
+            end_date += timedelta(days=1)
+            and_filters.append(CreditCardTransactionsLedger.created_at.between(start_date, end_date))
+
+        if query: 
+            q = f'%{query}%'
+
+            text_filters = db.or_(
+                (CreditCardTransactionsLedger.amount.ilike(q)),
+                (CreditCardTransactionsLedger.before_update_balance.ilike(q)),
+                (CreditCardTransactionsLedger.after_update_balance.ilike(q)),
+                (CreditCardTransactionsLedger.reference_code.ilike(q)),
+                (CreditCardTransactionsLedger.transaction_type.ilike(q)),
+                (CreditCardTransactionsLedger.created_at.ilike(q)),
+                (CreditCard.nick_name.ilike(q))
+            )
+
+            and_filters.append(text_filters)
+
+        ledgers = (
+            CreditCardTransactionsLedger.query
+            .outerjoin(CreditCardTransactionsLedger.credit_card)
+            .filter(db.and_(*and_filters))
+            .order_by(CreditCardTransactionsLedger.created_at.desc())
+            .all()
+        )
+
+        ledgers_list = []
+        for l in ledgers:
+            ledgers_list.append(l.to_dict())
+        
+        return jsonify({
+            'ledgers': ledgers_list,
+            'total': total_amount(ledgers)
+        }), 200
+    except Exception as e:
+        db.session.rollback()
+        traceback.print_exc()
+        return jsonify({'error': 'Internal server error'}), 500   

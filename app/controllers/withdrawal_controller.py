ed6487020d232ac1b64d030cd3cd4e39e5ed9ab2
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-01-27 21:38:26 -0400

    new controller for filtering withdrawals by input text and time frame at the same time

--- app/controllers/withdrawal_controller.py
+++ app/controllers/withdrawal_controller.py
@@ -128,6 +128,58 @@ def filter_withdrawals_by_field(query):
         traceback.print_exc()
         raise e
     
+def filter_all():
+    try:
+        data = request.get_json(silent=True) or {}
+
+        query = data.get('query')
+        start = data.get('start')
+        end = data.get('end')
+
+        if not query and (not start or not end):
+            return jsonify({
+                'error': 'Try to type some query or select a time frame.'
+            }), 400
+
+        and_filters = []
+
+        if start and end:
+            start_date = datetime.strptime(start, '%Y-%m-%d')
+            end_date = datetime.strptime(end, '%Y-%m-%d')
+            end_date += timedelta(days=1)
+            and_filters.append(Withdrawal.created_at.between(start_date, end_date))
+
+        if query: 
+            q = f'%{query}%'
+
+            text_filters = db.or_(
+                (Withdrawal.amount.ilike(q)),
+                (Withdrawal.description.ilike(q)),
+                (BankAccount.nick_name.ilike(q))
+            )
+            and_filters.append(text_filters)
+
+        withdrawals = (
+            Withdrawal.query
+            .outerjoin(Withdrawal.bank_account)
+            .filter(db.and_(*and_filters))
+            .order_by(Withdrawal.created_at.desc())
+            .all()
+        )
+
+        withdrawal_list = []
+        for l in withdrawals:
+            withdrawal_list.append(l.to_dict())
+        
+        return jsonify({
+            'withdrawals': withdrawal_list,
+            'total': total_amount(withdrawals)
+        }), 200
+    except Exception as e:
+        db.session.rollback()
+        traceback.print_exc()
+        return jsonify({'error': 'Internal server error'}), 500   
+    
 def filter_withdrawals_by_timeframe(start, end):
     try:
         if not start or not end:

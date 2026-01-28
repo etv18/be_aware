9a31a8419120229cd263dbeeeffc3f27356b03d9
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-01-28 15:10:17 -0400

    new controller for filtering deposits by input text and time frame

--- app/controllers/deposit_controller.py
+++ app/controllers/deposit_controller.py
@@ -145,7 +145,61 @@ def filter_by_field():
         }), 200
     except Exception as e:
         db.session.rollback()
-        raise e 
+        raise e
+    
+def filter_all():
+    try:
+        data = request.get_json(silent=True) or {}
+
+        query = data.get('query')
+        start = data.get('start')
+        end = data.get('end')
+
+        if not query and (not start or not end):
+            return jsonify({
+                'error': 'Try to type some query or select a time frame.'
+            }), 400
+
+        and_filters = []
+
+        if start and end:
+            start_date = datetime.strptime(start, '%Y-%m-%d')
+            end_date = datetime.strptime(end, '%Y-%m-%d')
+            end_date += timedelta(days=1)
+            and_filters.append(Deposit.created_at.between(start_date, end_date))
+
+        if query: 
+            q = f'%{query}%'
+
+            text_filters = db.or_(
+                (Deposit.amount.ilike(q)),
+                (Deposit.code.ilike(q)),
+                (Deposit.description.ilike(q)),
+                (Deposit.created_at.ilike(q)),
+                (BankAccount.nick_name.ilike(q))
+            )
+            and_filters.append(text_filters)
+
+        deposits = (
+            Deposit.query
+            .outerjoin(Deposit.bank_account) #allows to show expense without a bank account        
+            .filter(db.and_(*and_filters))
+            .order_by(Deposit.created_at.desc())
+            .all()
+        )
+
+        deposits_list = []
+        for l in deposits:
+            deposits_list.append(l.to_dict())
+        
+        return jsonify({
+            'deposits': deposits_list,
+            'total': total_amount(deposits)
+        }), 200
+    except Exception as e:
+        db.session.rollback()
+        traceback.print_exc()
+        return jsonify({'error': 'Internal server error'}), 500   
 
 def filter_by_time():
     try:

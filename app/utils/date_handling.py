2dedea2867dc5fa1b6942c0a12e2502e1945d3d1
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-01-29 13:00:26 -0400

    new constat with a list of all months

--- app/utils/date_handling.py
+++ app/utils/date_handling.py
@@ -1,4 +1,6 @@
 from datetime import datetime, timezone
 
+MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
 def utcnow():
     return datetime.now(timezone.utc)

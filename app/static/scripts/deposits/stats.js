963dcb566b732cced27c26c0aa0fd9778f6afcd0
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-02-02 13:05:05 -0400

    deposits  yearly line chart set up

---
+++ app/static/scripts/deposits/stats.js
@@ -0,0 +1,37 @@
+import { getData, generateYearlyAllModelsChart, CURRENT_YEAR } from '../utils/generateChart.js';
+
+const yearlyExpenseCanvas = document.getElementById('yearly-stats');
+const yearlySingleModelReportEndpoint = document.getElementById('yearly-single-model-report-endpoint').value;
+const chartTitle = document.getElementById('chart-title');
+const yearSelect = document.getElementById('year-select');
+const chartTitleTextContent = document.getElementById('chart-title').textContent;
+
+const BACKGROUND_COLOR = '#624ea2';
+const BORDER_COLOR = '#624ea2';
+const CHART_TYPE = 'line';
+const MODEL = 'deposits';
+
+document.addEventListener('DOMContentLoaded',async (event) => {
+    const yearlyStatsData = await getData(
+        yearlySingleModelReportEndpoint, 
+        {
+            model: MODEL,
+            year: CURRENT_YEAR
+        }
+    );
+    generateYearlyAllModelsChart(yearlyExpenseCanvas, CHART_TYPE, yearlyStatsData.label, yearlyStatsData[MODEL], BACKGROUND_COLOR, BORDER_COLOR);
+    chartTitle.textContent = chartTitleTextContent + ' - ' + CURRENT_YEAR;
+});
+
+yearSelect.addEventListener('change', async (event) => {
+    const selectedYear = event.target.value;
+    const yearlyStatsData = await getData(
+        yearlySingleModelReportEndpoint,
+        {
+            model: MODEL,
+            year: selectedYear
+        }
+    );
+    generateYearlyAllModelsChart(yearlyExpenseCanvas, CHART_TYPE, yearlyStatsData.label, yearlyStatsData[MODEL], BACKGROUND_COLOR, BORDER_COLOR);
+    chartTitle.textContent = chartTitleTextContent + ' - ' + selectedYear;
+});

69fbe1d8a64f0098e9e284634c92df2ac205f85e
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-02-06 21:54:43 -0400

    sql code updated

--- instance/update_datetime_columns.sql
+++ instance/update_datetime_columns.sql
@@ -1,10 +1,20 @@
 BEGIN TRANSACTION;
 
-UPDATE incomes
+UPDATE cash_ledger
 SET created_at = '2026-01-31 ' || substr(created_at, 12) --the space at the end is mandatory for updating the column
-WHERE date(created_at) = '2026-02-01';
+WHERE date(created_at) = '2026-02-01'
+AND type = 'EXPENSE'
+;
 
 SELECT id, amount, created_at
 FROM incomes
 WHERE date(created_at) = '2026-02-01';
+
+--select cash ledger table
+SELECT *
+FROM cash_ledger
+WHERE date(created_at) = '2026-01-31'
+AND type = 'EXPENSE';
+
+ROLLBACK;
 

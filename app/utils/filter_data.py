ce6dc554d55f0acc2022b7f2032237f0e7acd675
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-02-06 21:54:28 -0400

    util function to filter data yearly per column

--- app/utils/filter_data.py
+++ app/utils/filter_data.py
@@ -42,6 +42,50 @@ def get_yearly_total_amount_info(CustomModel, year=None) -> list:
         traceback.print_exc()
         raise e
 
+def get_yearly_total_amount_info_using_source(CustomModel, year=None, source=None) -> list:
+    try:
+        #This will return a yearly report of records which used either a credit card or a bank account
+        if year is None:
+            year = datetime.now().year
+
+        query = CustomModel.query.filter(
+            db.func.extract('year', CustomModel.created_at) == year
+        )
+
+        # Apply exclusive source filter
+        if source == "bank":
+            query = query.filter(CustomModel.bank_account_id.isnot(None))
+        elif source == "card":
+            query = query.filter(CustomModel.credit_card_id.isnot(None))
+        elif source == 'cash':
+            query = query.filter(CustomModel.is_cash.isnot(False))
+
+
+        results = (
+            query
+            .with_entities(
+                db.func.extract('month', CustomModel.created_at).label('month'),
+                db.func.sum(CustomModel.amount).label('total')
+            )
+            .group_by('month')
+            .all()
+        )
+
+        # Initialize all 12 months with 0
+        monthly_totals = {month: Decimal('0.00') for month in range(1, 13)}
+
+        # Fill months that have data
+        for month, total in results:
+            monthly_totals[int(month)] = total or Decimal('0.00')
+
+        return [monthly_totals[m] for m in range(1, 13)]
+
+    except Exception:
+        db.session.rollback()
+        traceback.print_exc()
+        raise
+
+
 def get_monthly_total_amount_info(CustomModel, year, month) -> Decimal:
     try:
         return CustomModel.query.filter(

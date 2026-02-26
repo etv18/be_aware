8ec2a6c24115c96da8e6601c70ed0954821df30f
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-02-26 17:53:41 -0400

    added debts and loans count for active records of each tables

--- app/routes/home_routes.py
+++ app/routes/home_routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, request, jsonify
 
 from app.controllers import home_controller
+from app.models.debt import Debt
+from app.models.loan import Loan
 from app.utils.date_handling import get_years
 
 home_bp = Blueprint('home', __name__)
@@ -8,8 +10,13 @@ home_bp = Blueprint('home', __name__)
 @home_bp.route('/', methods=['GET'])
 def index():
     years = get_years()
+    active_loans = Loan.query.filter(Loan.is_active == True).count()
+    active_debts = Debt.query.filter(Debt.is_active == True).count()
+
     context = {
         'years': years,
+        'active_loans': active_loans,
+        'active_debts': active_debts,
     }
     return render_template('home/index.html', **context)
 
@@ -23,7 +30,7 @@ def populate_monthly_expense_and_income_chart():
 def populate_yearly_and_income_chart():
     data = home_controller.get_yearly_income_and_expense_data()
     
-    return jsonify(data)
+    return jsonify(data) 
 
 
     

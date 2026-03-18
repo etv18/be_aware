857cb966753144fd6713f2676d8400e5162017f0
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-03-18 17:18:33 -0400

    refactored config file to use load env package

--- app/config.py
+++ app/config.py
@@ -1,7 +1,17 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Config:
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///be_aware.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     TEMPLATES_AUTO_RELOAD = True
+
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'America/Santo_Domingo'
+
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SECRET_KEY = os.getenv('SECRET_KEY')

a9b55ee713ced847aa0168132f2b82dc218dae73
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-03-20 17:28:39 -0400

    added CSRFProtect and LoginManager classes to extensions file

--- app/extensions.py
+++ app/extensions.py
@@ -2,7 +2,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_babel import Babel
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from flask_login import LoginManager
+from flask_wtf import CSRFProtect
 
 db = SQLAlchemy()
 babel = Babel()
 limiter = Limiter(key_func=get_remote_address)
+login_manager = LoginManager()
+crsf = CSRFProtect()

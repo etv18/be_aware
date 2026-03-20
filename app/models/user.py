4b708649a7fc93bff2ad8bdbb9543dbef2b56a6e
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-03-20 17:14:51 -0400

    User model set up

---
+++ app/models/user.py
@@ -0,0 +1,27 @@
+from app.extensions import db
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+from app.utils.date_handling import utcnow
+
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+    created_at = db.Column(
+        db.DateTime(timezone=True),
+        default=utcnow, #internally sqlalchemy will exectute the function so dont add the parentheses otherwise it'll break down when creating the record in the db
+        nullable=False
+    )
+    updated_at = db.Column(
+        db.DateTime(timezone=True), 
+        onupdate=utcnow, #internally sqlalchemy will exectute the function so dont add the parentheses otherwise it'll break down when creating the record in the db
+    )
+
+
+
+
+   
